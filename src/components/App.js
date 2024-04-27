8dd190d47b577554c70be1359e4d9b7b9bc38560
user.name <akamrannicole@gmail.com>
2024-04-27 16:35:29 +0300

    Component basics lab done

--- src/components/App.js
+++ src/components/App.js
@@ -16,15 +16,21 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
-
+function About ()
+{/* write an <About> component here */
+return (
+  <div id="home">
+    <h2>About</h2>
+  </div>
+);
+}
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home></Home>
+      <About></About>
     </div>
   );
 }
